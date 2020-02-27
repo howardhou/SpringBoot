0084da41961bf3cd715f99940a61fc072b682492
houdongdong <>
2020-02-27 17:45:35 +0800

    使用 nacos 配置

--- dubbo-demo/dubbo-demo-app-api/src/main/java/com/example/demo/appapi/DubboDemoAppApiApplication.java
+++ dubbo-demo/dubbo-demo-app-api/src/main/java/com/example/demo/appapi/DubboDemoAppApiApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo.appapi;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySource(dataId = "dubbo-demo-api", autoRefreshed = true)
 public class DubboDemoAppApiApplication {
 
     public static void main(String[] args) {
--- dubbo-demo/dubbo-demo-app-api/src/main/java/com/example/demo/appapi/UserController.java
+++ dubbo-demo/dubbo-demo-app-api/src/main/java/com/example/demo/appapi/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.appapi;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.example.demo.user.UserService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +13,13 @@ public class UserController {
     @Reference
     private UserService userService;
 
+    // 使用 @NacosValue 获取属性值
+    @NacosValue(value = "${company:none}", autoRefreshed = true)
+    private String company;
+
     @RequestMapping("/sayHello/{name}")
     public String sayHello(@PathVariable String name) {
 
-        return userService.sayHello(name);
+        return userService.sayHello(company + ": " + name);
     }
 }
