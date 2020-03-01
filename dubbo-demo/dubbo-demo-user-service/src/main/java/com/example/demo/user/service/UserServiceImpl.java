ab4c94991ecb714ebea6922d755a5060ce9aab57
houdongdong <>
2020-03-01 18:55:12 +0800

    使用Spring cloud nacos config

--- dubbo-demo/dubbo-demo-user-service/src/main/java/com/example/demo/user/service/UserServiceImpl.java
+++ dubbo-demo/dubbo-demo-user-service/src/main/java/com/example/demo/user/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.demo.user.UserService;
 import org.apache.dubbo.config.annotation.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 
 @Service
@@ -11,9 +12,19 @@ public class UserServiceImpl implements UserService {
 
     Logger logger = LoggerFactory.getLogger("Service");
 
+    @Value("${sleep:0}")
+    private int sleep;
+
     @Override
     public String sayHello(String name) {
-        logger.info("call sayHello");
+        logger.info("call sayHello, sleep: " + sleep);
+
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return "Hello, " + name + " (from Dubbo with Spring Boot)";
     }
 }
