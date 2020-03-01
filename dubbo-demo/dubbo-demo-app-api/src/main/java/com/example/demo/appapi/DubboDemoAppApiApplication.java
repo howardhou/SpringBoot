99442499f66cbae5b4b738b1751e29e67db29961
houdongdong <>
2020-03-01 18:30:19 +0800

    no message

--- dubbo-demo/dubbo-demo-app-api/src/main/java/com/example/demo/appapi/DubboDemoAppApiApplication.java
+++ dubbo-demo/dubbo-demo-app-api/src/main/java/com/example/demo/appapi/DubboDemoAppApiApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo.appapi;
 
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @NacosPropertySource(dataId = "dubbo-demo-api", autoRefreshed = true)
+@EnableDubbo
 public class DubboDemoAppApiApplication {
 
     public static void main(String[] args) {
