fdf92d579cd32fd5a3d4d3436e94323ae6bcb311
houdongdong <>
2020-03-26 22:57:00 +0800

    Gateway + Nacos 实现动态路由

---
+++ springcloud-demo/gateway/src/main/java/com/example/demo/gateway/NacosDynamicRouteService.java
@@ -0,0 +1,103 @@
+package com.example.demo.gateway;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+@Component
+public class NacosDynamicRouteService implements ApplicationEventPublisherAware {
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    private String dataId = "gateway-router";
+    private String group = "DEFAULT_GROUP";
+    @Value("${spring.cloud.nacos.config.server-addr}")
+    private String serverAddr;
+
+    @Autowired
+    private RouteDefinitionWriter routeDefinitionWriter;
+
+    private static final List<String> ROUTE_LIST = new ArrayList<>();
+
+    @PostConstruct
+    public void dynamicRouteByNacosListener() {
+        try {
+            ConfigService configService = NacosFactory.createConfigService(serverAddr);
+
+            String configInfo = configService.getConfig(dataId, group, 5000);
+            configRouter(configInfo);
+
+            configService.addListener(dataId, group, new Listener() {
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    configRouter(configInfo);
+                }
+
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+            });
+        } catch (NacosException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void configRouter(String configInfo){
+        clearRoute();
+
+        try {
+            List<RouteDefinition> gatewayRouteDefinitions = JSONObject.parseArray(configInfo, RouteDefinition.class);
+
+            for (RouteDefinition routeDefinition : gatewayRouteDefinitions) {
+                addRoute(routeDefinition);
+            }
+
+            publish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clearRoute() {
+        for(String id : ROUTE_LIST) {
+            this.routeDefinitionWriter.delete(Mono.just(id)).subscribe();
+        }
+
+        ROUTE_LIST.clear();
+    }
+
+    private void addRoute(RouteDefinition definition) {
+        try {
+            routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+            ROUTE_LIST.add(definition.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void publish() {
+        this.applicationEventPublisher.publishEvent(new RefreshRoutesEvent(this.routeDefinitionWriter));
+    }
+
+}
