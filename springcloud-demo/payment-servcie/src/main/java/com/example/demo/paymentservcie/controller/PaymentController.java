cf4c21d48e2e759fe66aa6507d650db502da6db3
houdongdong <>
2020-03-27 22:27:55 +0800

    解决共享配置无法获取数据的问题，使用bootstrap.properties属性文件

--- springcloud-demo/payment-servcie/src/main/java/com/example/demo/paymentservcie/controller/PaymentController.java
+++ springcloud-demo/payment-servcie/src/main/java/com/example/demo/paymentservcie/controller/PaymentController.java
@@ -2,8 +2,12 @@ package com.example.demo.paymentservcie.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.example.demo.common.dto.Balance;
 import com.example.demo.common.service.BalanceService;
+import org.apache.catalina.core.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +22,18 @@ import java.util.Map;
 @RestController
 @RefreshScope
 public class PaymentController implements BalanceService {
+
+    Logger logger = LoggerFactory.getLogger(PaymentController.class);
+
+    // 在项目的属性文件中配置
     @Value("${sleep:0}")
     private int sleep;
+
+    // 在共享配置文件中配置
+    @Value("${share.message:none}")
+    private String message;
+    @Value("${share.ticket:0}")
+    private int ticket;
 
     final static Map<Integer, Balance> balanceMap = new HashMap() {{
         put(1, new Balance(1, 10, 1000));
@@ -32,7 +46,7 @@ public class PaymentController implements BalanceService {
     @RequestMapping(value = "/pay/balance", method = RequestMethod.GET)
     @SentinelResource(value = "protected-resource", blockHandler = "handleBlock")
     public Balance getBalance(@RequestParam("id") Integer id) {
-        System.out.println("request: /pay/balance?id=" + id + ", sleep: " + sleep);
+        logger.info("request: /pay/balance?id=" + id + ", sleep: " + sleep);
         if (sleep > 0) {
             try {
                 Thread.sleep(sleep);
@@ -44,7 +58,8 @@ public class PaymentController implements BalanceService {
             return balanceMap.get(id);
         }
 
-        return new Balance(0, 0, 0,"none");
+        ApplicationContext sx;
+        return new Balance(0, 0, ticket, message);
     }
 
     public Balance handleBlock(Integer id, BlockException e) {
